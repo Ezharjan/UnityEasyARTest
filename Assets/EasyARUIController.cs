966a8d1e03f626bf7f377e66ac90a77e0b494954
Alexander <ezharjan@aliyun.com>
2020-06-16 17:55:56 +0800

    added plane tracking

---
+++ Assets/EasyARUIController.cs
@@ -0,0 +1,19 @@
+﻿using Common;
+using easyar;
+using UnityEngine;
+
+namespace MotionTracking
+{
+    public class EasyARUIController : MonoBehaviour
+    {
+        public ARSession Session;
+        public TouchController TouchControl;
+        private VIOCameraDeviceUnion vioCamera;
+
+        private void Awake()
+        {
+            vioCamera = Session.GetComponentInChildren<VIOCameraDeviceUnion>();
+            TouchControl.TurnOn(TouchControl.gameObject.transform, Camera.main, true, true, true, true);
+        }
+    }
+}
